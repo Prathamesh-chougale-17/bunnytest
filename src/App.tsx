c8f50ee92340215f84fc071c95bab7826ec1726d
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-06-18 13:21:15 +0530

    Added the iframe to the page to render the bunny video with quality control

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* https://iframe.mediadelivery.net/play/256620/cdd20bb7-58fb-4175-92bc-b4ffad8a7bee */}
+      <iframe
+        src="https://iframe.mediadelivery.net/play/256620/cdd20bb7-58fb-4175-92bc-b4ffad8a7bee?autoplay=true"
+        loading="lazy"
+        style={{
+          border: "none",
+          overflow: "hidden",
+          width: "100wv",
+          height: "100vh",
+        }}
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+        allowFullScreen={true}
+      ></iframe>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
